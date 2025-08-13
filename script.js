4923486696d371332e6efc15e69408c143cf7c16
MystRiim <157691471+MystRiim@users.noreply.github.com>
2025-08-13 12:41:25 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function changeCatFact() {
+    document.getElementById("catFact").innerHTML =
+    `Cats use their long tails to maintain balance, allowing them to walk on various narrow surfaces, 
+    like the tops of doors or fences. Their tail movements can also communicate different emotions, 
+    such as interest, friendliness, and fear. Cats are one of the most popular pets on the planet, 
+    but unlike dogs, humans havent had a large hand in selectively breeding the species. 
+    Instead, we developed a relationship with cats over thousands of years thanks to our need to stop 
+    rodents from eating our food and cats natural desire to catch and eat rodents.`;
+}
